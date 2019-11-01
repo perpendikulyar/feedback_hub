ccae2372ef9da6b44ece1a645d0f67900bfb25b3
Michael Nikolashin <mnikolashin@woman.ru>
2019-11-01 16:56:06 +0300

    [+] fix bug with still active early recieved userToken for inactive user

--- src/system-user/jwt.strategy.ts
+++ src/system-user/jwt.strategy.ts
@@ -1,18 +1,21 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SystemUserRepository } from './system-user.repository';
 import { JwtPayload } from './jwt-payload.interface';
 import { SystemUser } from './system-user.entity';
 
 import * as config from 'config';
+import { SystemUserStatus } from './system-user-status.enum';
 
 const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  private readonly logger = new Logger('Jwt strategy');
+
   constructor(
     @InjectRepository(SystemUserRepository)
     private systemUserRepository: SystemUserRepository,
@@ -27,10 +30,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     const { email } = payload;
     const systemUser = await this.systemUserRepository.findOne({ email });
 
-    if (!systemUser) {
+    this.logger.debug(JSON.stringify(systemUser.status));
+    if (!systemUser || systemUser.status === SystemUserStatus.INACTIVE) {
+      this.logger.debug('JWT returnn an anautorized');
       throw new UnauthorizedException();
     }
-
+    this.logger.debug('JWT return a user');
     return systemUser;
   }
 }
